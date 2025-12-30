3560f69b664295f134f3b4b23117df8f9a11259a
Reed Koser <248901343+rkoser-nv@users.noreply.github.com>
2025-12-29 22:30:05 -0800

    Fix ASTBuilder::getMemberDeclRef for type packs
    This fixes an assert when printing parameter as part of doc
    generation, where the failure to forward a DeclRef to a
    GenericTypePackParamDecl resulted in a malformed DeclRef.
    
    The problematic declaration is from hlsl.meta.slang:26829
      __init<each U : __BuiltinArithmeticType>(expand each U args)
    where the type of `args` had the malformed DeclRef type.

--- source/slang/slang-ast-builder.h
+++ source/slang/slang-ast-builder.h
@@ -329,7 +329,7 @@ public:
         if (!parent)
             return getDirectDeclRef(memberDecl);
         // A Generic value/type ParamDecl is always referred to directly.
-        if (as<GenericTypeParamDecl>(memberDecl) || as<GenericValueParamDecl>(memberDecl))
+        if (as<GenericTypeParamDeclBase>(memberDecl) || as<GenericValueParamDecl>(memberDecl))
             return getDirectDeclRef(memberDecl);
         if (as<ThisTypeDecl>(memberDecl) && !as<InterfaceDecl>(memberDecl->parentDecl))
             return as<T>(parent);
